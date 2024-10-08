a8b3b8f200ab40e08fcd7297bab0aa1ca6998f4c
Tapendra Shahi <tapendrashahi61@gmail.com>
2024-10-08 21:53:35 +0545

    Implemented function for food generation.

--- src/food.rs
+++ src/food.rs
@@ -1,5 +1,7 @@
 use macroquad::color::Color;
 
+use crate::grid::Grid;
+
 pub struct Food {
 	pub pos: (i32, i32),
 	pub color: Color,
@@ -7,11 +9,19 @@ pub struct Food {
 
 
 pub struct FoodGenerator {
+	food_container: Option<Food>,
 	food_exists: bool,
 }
 
 impl FoodGenerator {
 	pub fn new () -> Self {
-		FoodGenerator { food_exists: false }
+		FoodGenerator { food_container:None, food_exists: false }
+	}
+
+	pub fn generate_food (&mut self, grid_handler: &Grid) {
+		if !self.food_exists {
+			let food = Food { pos: grid_handler.generate_rand_coordinate(), color: Color::new(0.3, 0.0, 0.0, 1.0)};
+			self.food_container = Some(food);
+		}
 	}
 }
